6b81597c6165e27cb3bc021f2ed4a43e83cc6a5d
Moosees <linus.almgren@gmail.com>
2019-11-24 00:38:32 +0100

    Fix mongodb deprecation warnings

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ require('dotenv').config();
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(() => {
     console.log('MongoDB connected');
